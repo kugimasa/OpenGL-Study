c5c965c3fc5e659d715de80c6b43432d992fc0ae
kugi <kugimania@gmail.com>
2020-01-25 00:11:21 +0900

    GLUT-Intro/Chapter4

--- Intro/main.cpp
+++ Intro/main.cpp
@@ -2,13 +2,24 @@
 
 void display(void)
 {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
 }
+
+void initBackGroundColor(void)
+{
+    ///The color I like
+    glClearColor((double)120/255, (double)240/255, (double)80/255,1.0);
+}
+
 
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-    glutCreateWindow("Intro");
+    glutInitDisplayMode(GLUT_RGBA);
+    glutCreateWindow("Window");
     glutDisplayFunc(display);
+    initBackGroundColor();
     glutMainLoop();
     return 0;
 }
