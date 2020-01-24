2db964269503c3a60317491f202e9888309717ee
kugi <kugimania@gmail.com>
2020-01-25 00:37:09 +0900

    GLUT-Intro/Chapter5

--- Intro/main.cpp
+++ Intro/main.cpp
@@ -3,6 +3,16 @@
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glColor3d(1.0, 0.0, 0.0);
+    glVertex2d(-0.9,-0.9);
+    glColor3d(0.0, 1.0, 0.0);
+    glVertex2d(0.9,-0.9);
+    glColor3d(0.0, 0.0, 1.0);
+    glVertex2d(0.9,0.9);
+    glColor3d(1.0, 1.0, 0.0);
+    glVertex2d(-0.9,0.9);
+    glEnd();
     glFlush();
 }
 
@@ -17,7 +27,7 @@ int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA);
-    glutCreateWindow("Window");
+    glutCreateWindow("2D-Draw");
     glutDisplayFunc(display);
     initBackGroundColor();
     glutMainLoop();
