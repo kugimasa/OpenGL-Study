41517b6c2d742bb6b19a77c554f4704e5454ccf9
kugi <kugimania@gmail.com>
2020-01-27 23:26:29 +0900

    GLUT-Intro/Chapter7(WIP) Draw with Lines2

--- Intro/main.cpp
+++ Intro/main.cpp
@@ -1,46 +1,58 @@
 #include <iostream>
 #include <GLUT/glut.h>
 
+#define MAXPOINTS 100
+GLint points[MAXPOINTS][2];
+int point_num = 0;
+
+
 void Display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    /// Draw all the lines
+    if (point_num > 1)
+    {
+        /// Stroke Color (White)
+        glColor3d(0.0, 0.0, 0.0);
+        glBegin(GL_LINES);
+        for (int i = 0; i < point_num; ++i)
+        {
+            glVertex2iv(points[i]);
+        }
+        glEnd();
+    }
+
     glFlush();
 }
 
 void Resize(int w, int h)
 {
-//    const float basic_width = 200.0;
-//    const float basic_height = 200.0;
-    const float basic_width = 1.0;
-    const float basic_height = 1.0;
     /// The origin of viewport(mouse) in world coordinate
     const float origin_x = -0.5;
     const float origin_y = -0.5;
     glViewport(0, 0, w, h);
     glLoadIdentity();
-    glOrtho(origin_x / basic_width , (w + origin_x) / basic_width, h + origin_y / basic_height , origin_y / basic_height, -1.0, 1.0);
+    glOrtho(origin_x, w + origin_x, h + origin_y, origin_y, -1.0, 1.0);
 }
 
 void Mouse(int button, int state, int x, int y)
 {
-    static int point_x, point_y;
-    std::string direction;
     if (button == GLUT_LEFT_BUTTON)
     {
+        points[point_num][0] = x;
+        points[point_num][1] = y;
         if (state == GLUT_UP)
         {
             /// Stroke Color (White)
             glColor3d(0.0, 0.0, 0.0);
             glBegin(GL_LINES);
-            glVertex2i(point_x, point_y);
-            glVertex2i(x, y);
+            glVertex2iv(points[point_num - 1]);
+            glVertex2iv(points[point_num]);
             glEnd();
             glFlush();
-        } else {
-            /// Remember point clicked
-            point_x = x;
-            point_y = y;
         }
+        if (point_num < MAXPOINTS - 1) ++point_num;
     }
 }
 
