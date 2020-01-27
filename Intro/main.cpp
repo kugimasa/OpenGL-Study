c48aa00cd5cd446ee03044602425f8a81260849a
kugi <kugimania@gmail.com>
2020-01-27 22:38:46 +0900

    GLUT-Intro/Chapter7(WIP)

--- Intro/main.cpp
+++ Intro/main.cpp
@@ -1,28 +1,44 @@
+#include <iostream>
 #include <GLUT/glut.h>
 
 void Display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-    glColor3d(1.0, 0.0, 0.0);
-    glVertex2d(-0.9,-0.9);
-    glColor3d(0.0, 1.0, 0.0);
-    glVertex2d(0.9,-0.9);
-    glColor3d(0.0, 0.0, 1.0);
-    glVertex2d(0.9,0.9);
-    glColor3d(1.0, 1.0, 0.0);
-    glVertex2d(-0.9,0.9);
-    glEnd();
     glFlush();
 }
 
 void Resize(int w, int h)
 {
-    const float basic_width = 200.0;
-    const float basic_height = 200.0;
+//    const float basic_width = 200.0;
+//    const float basic_height = 200.0;
+    const float basic_width = 1.0;
+    const float basic_height = 1.0;
+    /// The origin of viewport(mouse) in world coordinate
+    const float origin_x = -0.5;
+    const float origin_y = -0.5;
     glViewport(0, 0, w, h);
     glLoadIdentity();
-    glOrtho(-w / basic_width, w / basic_width, -h / basic_height , h / basic_height, -1.0, 1.0);
+    glOrtho(origin_x / basic_width , (w + origin_x) / basic_width, h + origin_y / basic_height , origin_y / basic_height, -1.0, 1.0);
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+    std::string direction;
+    switch (button)
+    {
+        case GLUT_RIGHT_BUTTON:
+            direction = "right";
+            break;
+        case GLUT_LEFT_BUTTON:
+            direction = "left";
+            break;
+        case GLUT_MIDDLE_BUTTON:
+            direction = "middle";
+            break;
+        default:
+            break;
+    }
+    std::cout << direction + ": " << x << ":" << y << std::endl;
 }
 
 void InitBackGroundColor(void)
@@ -34,13 +50,14 @@ void InitBackGroundColor(void)
 
 int main(int argc, char *argv[])
 {
-    glutInitWindowPosition(100, 100);
+    glutInitWindowPosition(0, 0);
     glutInitWindowSize(320, 240);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA);
-    glutCreateWindow("Viewport");
+    glutCreateWindow("MouseInput");
     glutDisplayFunc(Display);
     glutReshapeFunc(Resize);
+    glutMouseFunc(Mouse);
     InitBackGroundColor();
     glutMainLoop();
     return 0;
