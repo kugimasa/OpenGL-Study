926d9bc0c78453ae83dbc15343a01dfa8e3506f2
kugi <kugimania@gmail.com>
2020-02-09 01:28:16 +0900

    GLUT-Intro/Chapter8

--- Intro/draw3D.cpp
+++ Intro/draw3D.cpp
@@ -9,9 +9,6 @@ void Display(void)
     glClear(GL_COLOR_BUFFER_BIT);
     /// The Color I Like
     glColor3d((double)120/255, (double)240/255, (double)80/255);
-    glTranslated(0.0, 0.0, -5.0);
-//    glRotated(25.0,0.0 ,1.0 , 0.0);
-//    glRotated(25.0,1.0 ,0.0 , 0.0);
     glutWireCube(1.0);
     glEnd();
     glFlush();
@@ -30,6 +27,15 @@ void Resize(int w, int h)
 
     ///// Perspective Projection
     gluPerspective(30.0, (double)w / (double)h, 1.0, 1000.0);
+
+    /// Parameters
+    GLdouble eye_pos[3] = {3.0, 4.0, 5.0};
+    GLdouble point_pos[3] = {0.0, 0.0, 0.0};
+    GLdouble axis_y[3] = {0.0, 1.0, 0.0};
+
+    gluLookAt(eye_pos[0], eye_pos[1], eye_pos[2],
+            point_pos[0], point_pos[1], point_pos[2],
+            axis_y[0], axis_y[1], axis_y[2]);
 }
 
 
