e8fcf19c0de1e17c1503b78000b0681cd590c0cc
kugi <kugimania@gmail.com>
2020-04-02 22:48:01 +0900

    GLUT-Intro/Chapter10(WIP) Using Depth Buffer

--- Intro/hidden_surface_removal.cpp
+++ Intro/hidden_surface_removal.cpp
@@ -1,4 +1,38 @@
-//
-// Created by 釘田尚弥 on 2020/04/01.
-//
+#include <GLUT/glut.h>
+#include "animation.hpp"
 
+const float fovy = 30.0;
+
+void TeaPots(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    gluLookAt(3.0, 4.0, 5.0,
+              0.0, 0.0, 0.0,
+              0.0, 1.0, 0.0);
+
+    glTranslated(0.5, 0.0, 0.0);
+    glColor3d(0, 1, 0);
+    glutWireTeapot(1.0);
+
+    glTranslated(-1.0, 0.0, 0.0);
+    glColor3d(1, 0, 0);
+    glutWireTeapot(1.0);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("HiddenSurfaceRemoval");
+    glutDisplayFunc(TeaPots);
+    glutReshapeFunc(Resize);
+    glutKeyboardFunc(KeyInput);
+    InitBackGroundColor();
+    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
+    return 0;
+}
