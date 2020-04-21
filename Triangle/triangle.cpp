f84d177e7dfa3b5fb9a027d640cf21b2fa217b0e
kugi <kugimania@gmail.com>
2020-04-21 23:22:44 +0900

    🔼 Draw Triangle

--- Triangle/triangle.cpp
+++ Triangle/triangle.cpp
@@ -13,5 +13,13 @@ void Hover(int x, int y)
 void Display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glColor3d(1.0, 1.0, 1.0);
+    glVertex2d(-0.5, 0.0);
+    glVertex2d( 0.5, 0.0);
+    glVertex2d( 0.0, 0.5);
+    glEnd();
+
     glFlush();
 }
