0d94648109c8a0bf6f62a95cccea6fecc9c8a5b3
kugi <kugimania@gmail.com>
2020-04-22 22:05:24 +0900

    👀 Change Size

--- Triangle/triangle.cpp
+++ Triangle/triangle.cpp
@@ -1,25 +1,56 @@
 #include "triangle.hpp"
 
+Triangle::Triangle()
+{
+    m_Size = 1.0;
+}
+
+Triangle::Triangle(double size)
+{
+    m_Size = size;
+}
+
+Triangle::Triangle(double size, double posX, double posY)
+{
+    m_Size = size;
+    SetVertex(posX, posY);
+}
+
+void Triangle::SetVertex(double posX, double posY)
+{
+    for (auto & index : m_Vertex) {
+        index[0] = m_Size * index[0] + posX;
+        index[1] = m_Size * index[1] + posY;
+    }
+}
+
 void Hover(int x, int y)
 {
+
     double posX =   2.0 * ( x / (double) m_Width)  - 1.0;
     double posY = - 2.0 * ( y / (double) m_Height) + 1.0;
+    static double size = 0.0;
+    static double step = 0.05;
+    Triangle triangle(size, posX, posY);
 
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.0, 0.0, 0.0, 1.0);
 
     glBegin(GL_TRIANGLES);
     glColor3d(1.0, 1.0, 1.0);
-    glVertex2d( cos(PI/2) + posX, sin(PI/2) + posY);
-    glVertex2d( cos(7 * PI/6) + posX,  sin(7 * PI/6) + posY);
-    glVertex2d( cos(11 * PI/6) + posX, sin(11 * PI/6) + posY);
+    for (auto & vertex : triangle.m_Vertex) {
+        glVertex2dv(vertex);
+    }
     glEnd();
+    glutSwapBuffers();
 
-    glFlush();
+    size += step;
+    if (size > 1.0) step = - 0.025;
+    if (size < 0.0) step =   0.1;
 }
 
-void Display(void)
+void Display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
+    glutSwapBuffers();
 }
--- Triangle/triangle.hpp
+++ Triangle/triangle.hpp
@@ -7,11 +7,28 @@
 
 using namespace std;
 
+static const double PI = 3.141592653589793;
 const int m_Width = 500;
 const int m_Height = 500;
-static const double PI = 3.141592653589793;
+
+class Triangle
+{
+public:
+    Triangle();
+    Triangle(double size);
+    Triangle(double size, double posX, double posY);
+    void SetSize(double size);
+    double GetSize();
+    void SetVertex(double posX, double posY);
+
+    double m_Size;
+    double m_Vertex[3][2] = {
+            {cos(PI/2), sin(PI/2)},
+            {cos(7 * PI/6), sin(7 * PI/6)},
+            {cos(11 * PI/6), sin(11 * PI/6)}
+    };
+};
+#endif //TRIANGLE_TRIANGLE_HPP
 
 void Hover(int x, int y);
 void Display(void);
-
-#endif //TRIANGLE_TRIANGLE_HPP
